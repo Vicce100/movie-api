3bf66b8c70d519d657744bf7d815b1ba9dae7639
Victor Eriksson <victor.eriksson03@gmail.com>
2022-05-30 22:10:18 +0200

    removed .js files thet got generated from compailing typescript files and added .js to gitignore

--- src/utilities/generateAccessToken.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable import/named */
-import jwt from 'jsonwebtoken';
-export const generateAccessToken = (user) => 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, {
-    expiresIn: '365d',
-});
